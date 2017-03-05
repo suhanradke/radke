1a7c15a8e0b1e8948b7668ef20d3258b9ac9076b
Avishkar Tamang <avishkar.tamang@selu.edu>
2017-03-05 00:45:42 -0800

    updatedFurnituresCRUDandFurnitureFrontController

---
+++ OppositeEnds/Controllers/FurnitureFrontController.cs
@@ -0,0 +1,34 @@
+﻿using OppositeEnds.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OppositeEnds.Controllers
+{
+    public class FurnitureFrontController : Controller
+    {
+        private OppositeEndsContext _Context;
+
+        public FurnitureFrontController()
+        {
+
+            _Context = new OppositeEndsContext();
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+
+        // GET: FurnitureFront
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OppositeEnds/Controllers/FurnituresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OppositeEnds.Models;
+
+namespace OppositeEnds.Controllers
+{
+    public class FurnituresController : Controller
+    {
+        private OppositeEndsContext db = new OppositeEndsContext();
+
+        // GET: Furnitures
+        public ActionResult Index()
+        {
+            return View(db.Furnitures.ToList());
+        }
+
+        // GET: Furnitures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Furniture furniture = db.Furnitures.Find(id);
+            if (furniture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(furniture);
+        }
+
+        // GET: Furnitures/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Furnitures/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Price,Quantity,Category,Picture")] Furniture furniture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Furnitures.Add(furniture);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(furniture);
+        }
+
+        // GET: Furnitures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Furniture furniture = db.Furnitures.Find(id);
+            if (furniture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(furniture);
+        }
+
+        // POST: Furnitures/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Price,Quantity,Category,Picture")] Furniture furniture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(furniture).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(furniture);
+        }
+
+        // GET: Furnitures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Furniture furniture = db.Furnitures.Find(id);
+            if (furniture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(furniture);
+        }
+
+        // POST: Furnitures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Furniture furniture = db.Furnitures.Find(id);
+            db.Furnitures.Remove(furniture);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
