e97f4f24f2ed2ce7bc57e85641da97361d28b852
Avishkar Tamang <avishkar.tamang@selu.edu>
2017-03-04 17:58:09 -0800

    Initiated Furniture model

---
+++ OppositeEnds/Models/Furniture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OppositeEnds.Models
+{
+    public class Furniture
+    {
+        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+        
+        public string Category { get; set; }
+
+        public string Picture { get; set; }
+
+    }
+}
