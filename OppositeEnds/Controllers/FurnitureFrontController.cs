df90cf49e051cc3931b760f50625fcf25093f0ab
Avishkar Tamang <avishkar.tamang@selu.edu>
2017-04-06 14:54:04 -0500

    Paging, sorting and searching furniture phase1

--- OppositeEnds/Controllers/FurnitureFrontController.cs
+++ OppositeEnds/Controllers/FurnitureFrontController.cs
@@ -1,4 +1,6 @@
 ﻿using OppositeEnds.Models;
+using PagedList;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -22,13 +24,50 @@ namespace OppositeEnds.Controllers
 
 
         // GET: FurnitureFront
-       
-        public ActionResult Index()
-        {
 
-            var furnitures = db.furnitures.ToList();
-         
-            return View(furnitures);
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.PriceSortParm = sortOrder == "Price" ? "Price_desc" : "Price";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var furnitures = from s in db.furnitures
+                             select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                furnitures = furnitures.Where(s => s.Name.Contains(searchString)
+                                       || s.Category.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    furnitures = furnitures.OrderByDescending(s => s.Name);
+                    break;
+                case "Price":
+                    furnitures = furnitures.OrderBy(s => s.Price);
+                    break;
+                case "Price_desc":
+                    furnitures = furnitures.OrderByDescending(s => s.Price);
+                    break;
+                default:
+                    furnitures = furnitures.OrderBy(s => s.Name);
+                    break;
+            }
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(furnitures.ToPagedList(pageNumber, pageSize));
+
+
         }
 
         public ActionResult productdetails(int? id)
