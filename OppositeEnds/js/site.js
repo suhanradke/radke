d165460491396f4007afc49fa2634129fc38bb24
suhan adhiraki <suhanradke>
2017-04-24 04:09:17 -0500

    Home page updated

---
+++ OppositeEnds/js/site.js
@@ -0,0 +1,23 @@
+﻿
+var $sidebarAndWrapper = $("#adminNav,#swap2");
+
+$("#sidebarToggle").on("click", function () {
+    $sidebarAndWrapper.toggleClass("hide-sidebar");
+   
+
+    if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+        $(this).text("Show Sidebar");
+
+    } else {
+        $(this).text("Hide Sidebar");
+    }
+
+});
+
+
+//$("#sidebarToggle").click(function () {
+//    $("#adminNav").toggle();
+//    //debugger;
+//});
+
+  
